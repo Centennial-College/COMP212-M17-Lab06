29ef8507d3ebf0349ca9fdf542003ac258e08943
Kevin Ma <kbma.kevin@gmail.com>
2017-07-31 18:14:28 -0400

    Add project files.

---
+++ Kevin-Ma_Lab06_Ex01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kevin_Ma_Lab06_Ex01
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
